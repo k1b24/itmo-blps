1fe386fd2985ca1b07a8a3836a896be30456ddcc
Sergey Lazeev <s.lazeev@okko.tv>
2024-05-10 14:46:08 +0300

    add profiles for sper-bank

--- sper-bank/src/main/kotlin/ru/itmo/SperBankApplication.kt
+++ sper-bank/src/main/kotlin/ru/itmo/SperBankApplication.kt
@@ -1,9 +1,11 @@
 package ru.itmo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("ru.itmo.configuration")
 class SperBankApplication
 
 fun main(args: Array<String>) {
--- sper-bank/src/main/kotlin/ru/itmo/configuration/WebClientConfiguration.kt
+++ sper-bank/src/main/kotlin/ru/itmo/configuration/WebClientConfiguration.kt
@@ -1,12 +1,16 @@
 package ru.itmo.configuration
 
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
+@ConfigurationProperties("kachalka")
 class WebClientConfiguration {
 
+    lateinit var baseUrl: String
+
     @Bean
-    fun kachalkaWebClient(): WebClient = WebClient.builder().baseUrl("http://balancer").build()
+    fun kachalkaWebClient(): WebClient = WebClient.builder().baseUrl(baseUrl).build()
 }
